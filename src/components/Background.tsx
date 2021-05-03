2783bc6439d858dc4611516bc39c6385d69e8efe
Martin Kenarov <martin.kenarov@egt-digital.com>
2021-05-03 21:27:52 +0300

    added overlay

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -4,7 +4,12 @@ import styles from './Background.module.css';
 interface BackgroundProps {}
 
 const Background: React.FC<BackgroundProps> = (props) => {
-    return (<div className={styles.Img} style={{backgroundImage: `url(${process.env.PUBLIC_URL}${'/images/bg_desktop.jpg'})`}}></div>);
+    return (
+        <>
+            <div className={styles.Img} style={{backgroundImage: `url(${process.env.PUBLIC_URL}${'/images/bg_desktop.jpg'})`}}></div>
+            <div className={styles.overlay} style={{background: 'rgba(0,0,0,0.5)', backgroundImage:`url(${process.env.PUBLIC_URL}${'/images/overlay.png'}) repeat`}}></div>
+        </>
+    );
 }
 
 export default Background;
