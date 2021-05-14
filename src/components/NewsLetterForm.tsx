ed600dc97174709b7eeb2dcd3180f6c4d88dfbf0
Martin Kenarov <marto.kenarov@gmail.com>
2021-05-14 20:53:50 +0300

    change form interaction with formik

--- src/components/NewsLetterForm.tsx
+++ src/components/NewsLetterForm.tsx
@@ -1,38 +1,74 @@
-import React from 'react';
-import {useFormik} from 'formik';
-import * as Yup from 'yup';
-import styles from './NewsLetter.module.css';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import styles from "./NewsLetter.module.css";
 
 const EmailSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required')
+  email: Yup.string().email("Invalid email").required("Required"),
 });
 
-type NewsLetterFormProps = {}
+type NewsLetterFormProps = {};
 
-const NewsLetterForm:React.FC<NewsLetterFormProps> = () => {
+const NewsLetterForm: React.FC<NewsLetterFormProps> = () => {
+  const initialValues = {
+    email: "",
+  };
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-        },
-        validationSchema: {EmailSchema},
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
-        }
-    })
+  const validate = (values) => {
+    let errors = {email: ''};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Invalid Email";
+    }
+    return errors;
+  };
 
-    return (
-        <form className={styles['form']} onSubmit={formik.handleSubmit}>
-            <label className={styles['subtitle']} htmlFor="email">Notify Me When It's Ready</label>
-            <div className={styles['inner-form']}>
-                <div className={styles['email-form']}>
-                    <input id="email" name="email" type="email" placeholder="your email" onChange={formik.handleChange} value={formik.values.email}></input>
-                    <span className={styles['error']}>Email address is required</span>
-                </div>
-                <button type="submit">Subscribe</button>
+  const submitForm = (values) => {
+    console.log("Values: ", values);
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={submitForm}
+    >
+      {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty,
+        } = formik;
+        return (
+          <form className={styles["form"]} onSubmit={handleSubmit}>
+            <label className={styles["subtitle"]} htmlFor="email">
+              Notify Me When It's Ready
+            </label>
+            <div className={styles["inner-form"]}>
+              <div className={styles["email-form"]}>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="your email"
+                  onChange={handleChange}
+                  value={values.email}
+                ></input>
+                {errors.email !== '' && touched.email ? <span className={styles['error']}>Email address is required</span> : null}
+              </div>
+              <button type="submit">Subscribe</button>
             </div>
-        </form>
-    )
-}
+          </form>
+        );
+      }}
+    </Formik>
+  );
+};
 
 export default NewsLetterForm;
